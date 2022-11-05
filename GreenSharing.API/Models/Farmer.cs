670e6a05c813c8363545bb98b21a5f1ecd7d492f
Marcel Tawé <marcelsnews@gmail.com>
2022-11-05 17:14:35 -0400

    initial push with db creation enabled
    initial push with db creation enabled

---
+++ GreenSharing.API/Models/Farmer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenSharing.API.Models
+{
+    public class Farmer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public DateTime Availability { get; set; }
+
+        //FK
+        public Guid AccountId { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Green_Sharing
+
+EntityFrameworkCore\Add-Migration InitialCreate -Context GreenSharingContext -Project  GreenSharing.API -StartupProject GreenSharing.API -Verbose
